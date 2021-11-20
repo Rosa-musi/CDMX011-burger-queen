3c84ca6b9a26f4f498e98ff0d824fb46ac6053b8
Rosa-musi <nadamejor27@hotmail.com>
2021-11-20 15:32:10 -0600

    renderizado condicional de menú, nombres de mesero y cliente

---
+++ src/components/Pages/TakeOrder/TakeOrder.jsx
@@ -0,0 +1,107 @@
+import React, { useContext, useEffect } from 'react'
+import styled from 'styled-components'
+
+import Button from '../Button/Button'
+import colors from '../../../styles/colors'
+import { orderContext } from '../../../context/OrderContext'
+
+const Main = styled.main`
+    display: flex;
+    width: 95%;
+`
+const InputDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 60%;
+`
+const OptionsDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 50%;
+`
+const Title = styled.h2`
+    font-size: 30px;
+    margin: 20px;
+    border-bottom: 3px solid ${colors.yellow};
+`
+
+const Label = styled.label`
+    font-size: 15px;
+    font-weight: bold;
+`
+const SelectWaiter = styled.select`
+    margin: 20px;
+    background-color: #FFF8ED;
+    border-radius: 10px;
+    width: 200px;
+    height: 30px;
+    border: 1px solid black;
+`
+
+const InputCustomer = styled.input`
+    margin: 20px;
+    background-color: #FFF8ED;
+    border-radius: 10px;
+    width: 200px;
+    height: 25px;
+    border: 1px solid black;
+`
+const DataDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 40%;
+`
+const CustomersName = styled.p`
+    font-size: 30px;
+    margin: 20px;
+    font-weight: bold;
+    margin: 20px 0 30%;
+`
+
+export default function TakeOrder () {
+
+    const { waiter, setWaiter, customer, setCustomer } = useContext(orderContext)
+
+    function handleChangeWaiter (e) {
+        const { value } = e.target;
+        return(
+            setWaiter(value)
+        )
+    }
+
+    function handleChangeCustomer (e) {
+        const { value } = e.target;
+        return(
+            setCustomer(value)
+        )
+    }
+
+    return (
+        <Main>
+            <InputDiv>
+                <Title>Create order</Title>
+                <OptionsDiv> 
+                    <Label for="waiter">Waiter
+                        <SelectWaiter name="waiter" id="waiter" onChange={handleChangeWaiter} value={waiter}>
+                            <option value="Juan">Juan</option>
+                            <option value="Rosa">Rosa</option>
+                            <option value="Vania">Vania</option>
+                            <option value="Felipe">Felipe</option>
+                        </SelectWaiter>
+                    </Label> 
+                    <Label for="cName">Customer's name
+                        <InputCustomer type="text" id="cName" value={customer} onChange={handleChangeCustomer}/>
+                    </Label>
+                </OptionsDiv>
+            </InputDiv>
+            <DataDiv>
+                <CustomersName>{customer}</CustomersName>
+                <Button title="create"/>
+            </DataDiv>
+        </Main>
+    )
+}
